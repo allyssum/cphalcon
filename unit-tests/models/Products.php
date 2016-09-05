546ba37d03f12c33e8e470861c53581cb68667cd
朱宗鑫 <dreamsxin@qq.com>
2016-09-05 11:18:25 +0800

    Updated class Phalcon\Http\Client\Adapter\Curl

--- unit-tests/models/Products.php
+++ unit-tests/models/Products.php
@@ -1,10 +1,5 @@
 <?php
-	$configMysql = array(
-		'host' => 'localhost',
-		'username' => 'root',
-		'password' => '123456',
-		'dbname' => 'phalcon_test'
-	);
+
 class Products extends \Phalcon\Mvc\Model
 {
 
